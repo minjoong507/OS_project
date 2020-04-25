c296d4ba1cc5a89d05bc262680d5db26d32710c6
minjoong <albert507@naver.com>
2020-04-26 02:08:01 +0900

    쓰레드 사이즈 확인

--- projects/1/synctest.c
+++ projects/1/synctest.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <threads/thread.h>
 
 void synctest(void)
 {
 	printf("implement synchronize test!!!\n");
+	printf("size of struct thread : %d\n",sizeof(struct thread));
 }
