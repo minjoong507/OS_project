58017d67e2c7a682ef7f43c9a928a8bcf4cbe578
minjoong <albert507@naver.com>
2020-05-02 14:37:26 +0900

    파일 교체

--- projects/1/synctest.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <threads/thread.h>
-
-void synctest(void)
-{
-	printf("implement synchronize test!!!\n");
-	printf("size of struct thread : %d\n",sizeof(struct thread));
-}
--- projects/1/synctest.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SYNCTEST_H__
-#define _SYNCTEST_H__
-
-void synctest(void);
-
-#endif
